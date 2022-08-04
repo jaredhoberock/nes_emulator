38c62d2612c279ac0ce23332af8a95eb7bb02b11
Jared Hoberock <jaredhoberock@gmail.com>
2022-08-03 21:50:07 -0500

    Propagate nonmaskable interrupts from ppu to cpu

--- emulate.hpp
+++ emulate.hpp
@@ -10,8 +10,6 @@ inline void emulate(class system& sys, std::atomic<bool>& cancelled, std::atomic
   // this approach steps the cpu one instruction and then steps the ppu 3
   // times as many pixels as the number of cpu cycles consumed
 
-  auto frame_begin = std::chrono::high_resolution_clock::now();
-
   int ppu_cycle = 0;
   int cpu_cycle = sys.cpu().reset();
   for(int i = 0; i < 3 * cpu_cycle; ++i)
@@ -33,26 +31,18 @@ inline void emulate(class system& sys, std::atomic<bool>& cancelled, std::atomic
       // execute the next instruction
       int num_cpu_cycles = sys.cpu().step_instruction();
 
+      // execute any nonmaskable interrupt
+      if(sys.ppu().nmi)
+      {
+        num_cpu_cycles += sys.cpu().nonmaskable_interrupt();
+        sys.ppu().nmi = false;
+      }
+
       // let the ppu catch up to the cpu
       for(int i = 0; i < 3 * num_cpu_cycles; ++i)
       {
         sys.ppu().step_cycle();
         ++ppu_cycle;
-
-        // detect the end of a frame
-        // XXX get these numbers from elsewhere -- they are the number of pixels per frame
-        if(ppu_cycle == 341 * 261)
-        {
-          ppu_cycle = 0;
-
-          // how long did last frame take us?
-          auto frame_us = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - frame_begin);
-
-          // sleep until we need to start the next frame
-          std::this_thread::sleep_for(std::chrono::microseconds(16670) - frame_us);
-
-          frame_begin = std::chrono::high_resolution_clock::now();
-        }
       }
 
       cpu_cycle += num_cpu_cycles;
