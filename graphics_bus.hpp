0888643e2a71b238024b162ff27e74179da24a7d
Jared Hoberock <jaredhoberock@gmail.com>
2022-08-03 21:45:08 -0500

    Add nametables to graphics bus

--- graphics_bus.hpp
+++ graphics_bus.hpp
@@ -10,12 +10,16 @@ class graphics_bus
 {
   private:
     cartridge& cart_;
-    std::array<std::uint8_t, 32> pallete_;
+    // XXX the palette should maybe be moved to the ppu
+    std::array<std::uint8_t, 32> palette_;
+    // XXX these should go into a single array called vram
+    std::array<std::uint8_t, 1024> nametable_zero_;
+    std::array<std::uint8_t, 1024> nametable_one_;
 
   public:
     graphics_bus(cartridge& cart)
       : cart_{cart},
-        pallete_{}
+        palette_{}
     {}
 
     inline std::uint8_t read(std::uint16_t address) const
@@ -29,6 +33,55 @@ class graphics_bus
       else if(0x2000 <= address and address < 0x3F00)
       {
         // nametables
+        
+        // this bitwise AND does address mirroring
+        address &= 0x0FFF;
+        
+        // XXX this calculation should be done by the mapper
+        if(cart_.nametable_mirroring() == cartridge::horizontal)
+        {
+          if(0x0000 <= address and address < 0x0400)
+          {
+            // XXX what is this AND for?
+            result = nametable_zero_[address & 0x03FF];
+          }
+          else if(0x0400 <= address and address < 0x0800)
+          {
+            result = nametable_zero_[address & 0x03FF];
+          }
+          else if(0x0800 <= address and address < 0x0C00)
+          {
+            result = nametable_one_[address & 0x03FF];
+          }
+          else if(0x0C00 <= address and address < 0x1000)
+          {
+            result = nametable_one_[address & 0x3FF];
+          }
+        }
+        else if(cart_.nametable_mirroring() == cartridge::vertical)
+        {
+          if(0x0000 <= address and address < 0x0400)
+          {
+            // XXX what is this AND for?
+            result = nametable_zero_[address & 0x03FF];
+          }
+          else if(0x0400 <= address and address < 0x0800)
+          {
+            result = nametable_one_[address & 0x03FF];
+          }
+          else if(0x0800 <= address and address < 0x0C00)
+          {
+            result = nametable_zero_[address & 0x03FF];
+          }
+          else if(0x0C00 <= address and address < 0x1000)
+          {
+            result = nametable_one_[address & 0x3FF];
+          }
+        }
+        else
+        {
+          throw std::runtime_error("graphics_bus::read: Illegal nametable_mirroring_kind");
+        }
       }
       else if(0x3F00 <= address and address < 0x4000)
       {
@@ -38,7 +91,7 @@ class graphics_bus
         if(address == 0x0018) address = 0x0008;
         if(address == 0x001C) address = 0x000C;
 
-        result = pallete_[address];
+        result = palette_[address];
       }
       else
       {
@@ -53,6 +106,75 @@ class graphics_bus
       if(0x2000 <= address and address < 0x3F00)
       {
         // nametables
+
+        // this bitwise AND does address mirroring
+        address &= 0x0FFF;
+
+        // XXX this calculation should be done by the mapper
+        // we should first calculate the logical nt index
+        // and then map that to a physical nt index
+        // the logical nt index is just address / nametable_size_in_bytes
+
+        // XXX try this simpler calculation once we can see stuff on the screen
+        //int logical_nametable_idx = address / 1024;
+        //int physical_nametable_idx = (cart_.nametable_mirroring() == cartridge::horizontal) ?
+        //  logical_nametable_idx / 2 :
+        //  logical_nametable_idx % 2
+        //;
+
+        //// the offset into the nametable is just the address % 1024
+        //int nametable_offset address % 1024;
+
+        // XXX this calculation should be done by the mapper
+        if(cart_.nametable_mirroring() == cartridge::horizontal)
+        {
+          if(0x0000 <= address and address < 0x0400)
+          {
+            // logical nt 0 maps to physical nt 0
+
+            // XXX these ANDs compute an offset into each nametable, as above
+            nametable_zero_[address & 0x03FF] = data;
+          }
+          else if(0x0400 <= address and address < 0x0800)
+          {
+            // logical nt 1 maps to physical nt 0
+            nametable_zero_[address & 0x03FF] = data;
+          }
+          else if(0x0800 <= address and address < 0x0C00)
+          {
+            // logical nt 2 maps to physical nt 1
+            nametable_one_[address & 0x03FF] = data;
+          }
+          else if(0x0C00 <= address and address < 0x1000)
+          {
+            // logical nt 3 maps to physical nt 1
+            nametable_one_[address & 0x03FF] = data;
+          }
+        }
+        else if(cart_.nametable_mirroring() == cartridge::vertical)
+        {
+          if(0x0000 <= address and address < 0x0400)
+          {
+            // XXX what is this AND for?
+            nametable_zero_[address & 0x03FF] = data;
+          }
+          else if(0x0400 <= address and address < 0x0800)
+          {
+            nametable_one_[address & 0x03FF] = data;
+          }
+          else if(0x0800 <= address and address < 0x0C00)
+          {
+            nametable_zero_[address & 0x03FF] = data;
+          }
+          else if(0x0C00 <= address and address < 0x1000)
+          {
+            nametable_one_[address & 0x03FF] = data;
+          }
+        }
+        else
+        {
+          throw std::runtime_error("graphics_bus::read: Illegal nametable_mirroring_kind");
+        }
       }
       else if(0x3F00 <= address and address < 0x4000)
       {
@@ -62,7 +184,7 @@ class graphics_bus
         if(address == 0x0018) address = 0x0008;
         if(address == 0x001C) address = 0x000C;
 
-        pallete_[address] = data;
+        palette_[address] = data;
       }
       else
       {
