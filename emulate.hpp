bcc9345ada0d238c96f6a27dc984738455bbc8cb
Jared Hoberock <jaredhoberock@gmail.com>
2022-08-05 03:44:05 -0500

    Add single argument emulate function

--- emulate.hpp
+++ emulate.hpp
@@ -4,6 +4,45 @@
 #include <iostream>
 #include <thread>
 
+inline void emulate(class system& sys)
+{
+  // this approach steps the cpu one instruction and then steps the ppu 3
+  // times as many pixels as the number of cpu cycles consumed
+
+  int ppu_cycle = 0;
+  int cpu_cycle = sys.cpu().reset();
+  for(int i = 0; i < 3 * cpu_cycle; ++i)
+  {
+    sys.ppu().new_step_cycle();
+    ++ppu_cycle;
+  }
+
+  while(true)
+  {
+    // log current cpu state
+    sys.cpu().log(std::cout, cpu_cycle, ppu_cycle);
+
+    // execute the next instruction
+    int num_cpu_cycles = sys.cpu().step_instruction();
+
+    // execute any nonmaskable interrupt
+    if(sys.ppu().nmi)
+    {
+      num_cpu_cycles += sys.cpu().nonmaskable_interrupt();
+      sys.ppu().nmi = false;
+    }
+
+    // let the ppu catch up to the cpu
+    for(int i = 0; i < 3 * num_cpu_cycles; ++i)
+    {
+      sys.ppu().new_step_cycle();
+      ++ppu_cycle;
+    }
+
+    cpu_cycle += num_cpu_cycles;
+  }
+}
+
 
 inline void emulate(class system& sys, std::atomic<bool>& cancelled, std::atomic<bool>& paused, std::ostream& cpu_log, std::ostream& error_log)
 {
@@ -14,7 +53,7 @@ inline void emulate(class system& sys, std::atomic<bool>& cancelled, std::atomic
   int cpu_cycle = sys.cpu().reset();
   for(int i = 0; i < 3 * cpu_cycle; ++i)
   {
-    sys.ppu().step_cycle();
+    sys.ppu().new_step_cycle();
     ++ppu_cycle;
   }
 
@@ -26,7 +65,7 @@ inline void emulate(class system& sys, std::atomic<bool>& cancelled, std::atomic
       paused.wait(true);
 
       // log current cpu state
-      sys.cpu().log(cpu_log, cpu_cycle, ppu_cycle);
+      //sys.cpu().log(cpu_log, cpu_cycle, ppu_cycle);
 
       // execute the next instruction
       int num_cpu_cycles = sys.cpu().step_instruction();
@@ -36,12 +75,14 @@ inline void emulate(class system& sys, std::atomic<bool>& cancelled, std::atomic
       {
         num_cpu_cycles += sys.cpu().nonmaskable_interrupt();
         sys.ppu().nmi = false;
+
+//        std::this_thread::sleep_for(std::chrono::microseconds(16667));
       }
 
       // let the ppu catch up to the cpu
       for(int i = 0; i < 3 * num_cpu_cycles; ++i)
       {
-        sys.ppu().step_cycle();
+        sys.ppu().new_step_cycle();
         ++ppu_cycle;
       }
 
